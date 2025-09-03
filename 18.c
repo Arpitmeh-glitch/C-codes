8b435de30344bee9d0c4c0fe8d5375ec444905a8
Arpitmeh-glitch <mehrotraa245@gmail.com>
2025-09-03 11:34:08 +0530

    dgdfhf

--- 18.c
+++ 18.c
@@ -1,3 +1,29 @@
+// Q18: //Write a program that accepts a percentage (0-100) and assigns a grade based on the following criteria:
+//.
+
+/*
+Sample Test Cases:
+Input 1:
+95
+Output 1:
+Grade A
+
+Input 2:
+82
+Output 2:
+Grade B
+
+Input 3:
+68
+Output 3:
+Grade D
+
+Input 4:
+50
+Output 4:
+Grade F
+
+*/
 #include <stdio.h>
 int main()
 {
